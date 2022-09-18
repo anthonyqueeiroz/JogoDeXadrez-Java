322d186a8e285264b5f18947751d71b7dc291183
Anthony Queiroz <hanthonybdqt@gmail.com>
2022-09-18 18:53:41 -0400

    Created Classes Board and Piece with association between them

--- src/model/entities/boardgame/Position.java
+++ src/model/entities/boardgame/Position.java
@@ -28,11 +28,6 @@ public class Position {
 	public void setColumn(Integer column) {
 		this.column = column;
 	}
-
-	public void setValues(Integer row, Integer column) {
-		this.row=row;
-		this.column=column;
-	}
 	
 	@Override
 	public String toString() {
